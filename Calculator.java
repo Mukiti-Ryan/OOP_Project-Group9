fb0c7ad0e0f6ffeeb179e95374766412614fd7b6
Peter Wachira <69796027+petergwachira@users.noreply.github.com>
2024-02-19 21:49:37 +0300

    Add files via upload
    Initial Abstract Class Commit (Simple Arithmetic)

---
+++ Calculator.java
@@ -0,0 +1,6 @@
+public abstract class Calculator {
+    abstract double add(double a, double b);
+    abstract double subtract(double a, double b);
+    abstract double multiply(double a, double b);
+    abstract double divide(double a, double b);
+}
