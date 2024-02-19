b128bd23c3ced2393f11d6886d9b782e5d09529a
omar-903 <160486130+omar-903@users.noreply.github.com>
2024-02-19 22:02:14 +0300

    Add files via upload
    Scientific Operations Interface

---
+++ ScientificOperations.java
@@ -0,0 +1,16 @@
+public interface ScientificOperations {
+    double sin(double angle);
+    double cos(double angle);
+    double tan(double angle);
+    double sinh(double angle);
+    double cosh(double angle);
+    double tanh(double angle);
+    double log(double a);
+    double power(double base, double exponent);
+    double factorial(double a);
+    double modulo(double a, double b);
+    double sqrt(double a);
+    double cbrt(double a);
+    double pi(double a);
+    double inverse(double a);
+}
